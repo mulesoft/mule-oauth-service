7e30d5b57747df64899aa3b927f511f0f84a77bc
Mariano Rean <mrean@mulesoft.com>
2024-10-21 14:24:39 -0300

    fix/W-16452921: Set target bytecode level to J17 in ee services

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -13,7 +13,7 @@ import org.mule.api.annotation.jpms.ServiceModule;
  * @since 2.2
  */
 @ServiceModule
-module org.mule.service.oauth {
+module org.mule.test.service.oauth {
 
   requires org.mule.oauth.client.api;
   requires org.mule.oauth.client.impl;
@@ -23,15 +23,9 @@ module org.mule.service.oauth {
 
   requires java.inject;
 
-  // Allow invocation and injection into providers by the Mule Runtime
-  exports org.mule.service.oauth.provider to
-      org.mule.runtime.service;
-  opens org.mule.service.oauth.provider to
-      org.mule.runtime.service;
-
-  exports org.mule.service.oauth.internal to
-      com.mulesoft.mule.service.oauth.ee,
-      org.mule.test.service.oauth,
-      com.mulesoft.test.mule.service.oauth.ee;
-
+  requires org.mule.service.oauth;
+  requires org.hamcrest;
+  requires org.mockito;
+  requires junit;
+  requires io.qameta.allure.commons;
 }
